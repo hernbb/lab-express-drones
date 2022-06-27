7729976d93df719b28b847d8a7bd4b10c9c13d37
Hernando Bifaretti <hbifaretti@gmail.com>
2022-06-27 19:56:04 +0200

    done-iteration-5

--- routes/drones.js
+++ routes/drones.js
@@ -45,16 +45,44 @@ router.post('/drones/create', (req, res, next) => {
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
+  Drone.findById(req.params.id)
+  .then((response)=>{
+    // console.log(response)
+    res.render("drones/update-form.hbs", response)
+  })
+  .catch((e)=>{
+    console.log(e)
+  })
   // ... your code here
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
+  const name = req.body.name;
+  const propellers = req.body.propellers;
+  const maxSpeed = req.body.speed;
+  // const {name, propellers, maxSpeed} = req.body
+  Drone.findByIdAndUpdate(req.params.id, {name, propellers, maxSpeed})
+  .then(()=>{
+    res.redirect('/drones')
+  })
+  .catch((e)=>{
+    
+    res.render('drones/update-form.hbs')
+  })
   // ... your code here
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
+
+Drone.findByIdAndDelete(req.params.id )
+.then(()=>{
+  res.redirect('/drones')
+})
+.catch((e)=>{
+  alert(error)
+})
   // ... your code here
 });
 
