f49e72e6e0f24db5d6dfafd06000371e4196bfeb
Hernando Bifaretti <hbifaretti@gmail.com>
2022-06-27 18:25:17 +0200

    iteration-3-done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,25 @@
 // Iteration #1
+const mongoose = require("mongoose");
+
+// aqui creamos el Schema
+const droneSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+
+  propellers: {
+    type: Number,
+  },
+
+  maxSpeed: {
+    type: Number,
+  }
+  
+});
+
+// crear el Model
+const Drone = mongoose.model("drone", droneSchema);
+// mongoose.model(nombreDelModelo, schemaDelModelo)
+// "student" indica como se llamara nuestra colección => "students"
+
+module.exports = Drone;
--- routes/drones.js
+++ routes/drones.js
@@ -1,22 +1,47 @@
+const { response } = require('express');
 const express = require('express');
+const Drone = require('../models/Drone.model');
 const router = express.Router();
 
 // require the Drone model here
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
+  Drone.find()
+  .then((response)=>{
+
+    res.render('drones/list.hbs', {response})
+
+  })
+  .catch((e)=>{next(e)})
   // ... your code here
 });
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
+  
+    res.render('../views/drones/create-form.hbs')
+  })
+
   // ... your code here
-});
+
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  const name = req.body.name;
+  const propellers = req.body.propellers;
+  const maxSpeed = req.body.speed;
+
+  Drone.create({name:name, propellers:propellers, maxSpeed:maxSpeed})
+
+  .then(()=>{
+    res.redirect("/drones")
+  })
+  .catch(()=>
+  res.redirect('drones/create'))
 });
+  // ... your code here
+
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
